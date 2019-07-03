10bf526fff5de6b3c76565115f870c2260093a1e
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-07-04 03:37:33 +0900

    rama reader

---
+++ readmiscfile/read_rosettafiles.py
@@ -0,0 +1,40 @@
+
+def read_rama(filename):
+    from pandas import  DataFrame as df
+    ramafile=open(filename)
+    lines=ramafile.readlines()
+    #df.append()
+    ramaheader = ["resn","aa","ss","abego","phi","psi","omega"]
+    #df=df(columns=ramaheader)
+
+    resn=[]
+    aa=[]
+    ss=[]
+    abego=[]
+    phi=[]
+    psi=[]
+    omega=[]
+
+    for line in lines:
+        parsed=list(map(str, line.split()))
+        if ( parsed[0] == "#"):
+            print("header ignored")
+            print(parsed)
+        else:
+            resn += [parsed[0]]
+            aa += [parsed[1]]
+            ss += [parsed[2]]
+            abego += [parsed[3]]
+            phi += [parsed[4]]
+            psi += [parsed[5]]
+            omega += [parsed[6]]
+
+    return  df(
+            data={'resn':resn,
+                  'aa':aa,
+                  'ss':ss,
+                  'abego':abego,
+                  'phi':phi,
+                  "psi":psi,
+                  "omega":omega},
+            columns=ramaheader)
